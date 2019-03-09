3f57d3da5f2f924542861dcb399fbc4fcb86e36f
Hoang Tuan <hoanganhtuan.ou@gmail.com>
2019-03-09 12:30:37 +0700

    - updating CV.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var input_sass = 'src/sass/**/*.sass';
-var input_pug = 'src/jade/**/*.jade';
+var input_pug = 'src/views/**/*.pug';
 
 var output_sass = 'src/css';
 
@@ -12,56 +12,56 @@ var plumber = require('gulp-plumber');
 var pug = require('gulp-pug');
 
 gulp.task('sass', function() {
-	return gulp.src(input_sass)
-	.pipe(plumber({
-		errorHandler: function(error){
-			console.log(error.toString());
-			this.emit('end');
-		}
-	}))
-	.pipe(sass({
-		includePaths: ['./src/sass']
-	}))
-	.pipe(autoprefixer({
-		browser: ['last 2 versions']
-	}))
-	.pipe(cleanCSS({compatibility: 'ie8'}))
-	.pipe(gulp.dest(output_sass))
-	.pipe(browserSync.reload({
-		stream: true
-	}))
+  return gulp.src(input_sass)
+  .pipe(plumber({
+    errorHandler: function(error){
+      console.log(error.toString());
+      this.emit('end');
+    }
+  }))
+  .pipe(sass({
+    includePaths: ['./src/sass']
+  }))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions']
+  }))
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(gulp.dest(output_sass))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
 });
 
 gulp.task('pug',function(){
-	return gulp.src(input_pug)
-		.pipe(plumber({
-			errorHandler: function(error){
-				console.log(error.toString());
-				this.emit('end');
-			}
-		}))
-		.pipe(pug({
-			pretty: true
-		}))
-		.pipe(gulp.dest(''))
-		.pipe(browserSync.reload({
-			stream: true
-		}))
+  return gulp.src(input_pug)
+    .pipe(plumber({
+      errorHandler: function(error){
+        console.log(error.toString());
+        this.emit('end');
+      }
+    }))
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest(''))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 gulp.task('browserSync', function() {
-	browserSync.init({
-		server: {
-			baseDir: './'
-		}
-	})
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  })
 });
 
 gulp.task('watch', ['browserSync','sass','pug'], function() {
-	gulp.watch(input_sass, ['sass']);
-	gulp.watch(input_pug,['pug']);
-	gulp.watch('*.html', browserSync.reload);
-	gulp.watch('src/js/**/*.js', browserSync.reload);
+  gulp.watch(input_sass, ['sass']);
+  gulp.watch(input_pug,['pug']);
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('src/js/**/*.js', browserSync.reload);
 
-	//other watchers
+  //other watchers
 });
